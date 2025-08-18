a3c88ea65f521e896a403388d26def33bb52290a
Maruf2309 <maruf.hossain.1997@gmail.com>
2025-08-18 08:14:22 +0600

    oop1

--- Basic_Coding/fun_any.py
+++ Basic_Coding/fun_any.py
@@ -1,12 +1,12 @@
 
 enemies = [
     {'type':'Orc','health':0 },
-    {'type':'Orc', 'heath':0},
+    {'type':'Orc', 'health':0},
     {'type':'Orc', 'health':1},
     {'type':'Orc','health':2}
 ]
 
-if any([enemies['health'] for enemy in enemies]):
+if any([enemy['health'] for enemy in enemies]):
     print('The battle is not over')
 else:
     print('No more enemies remail!')
